4a34cd9d7995537e42c79aa271a5558098fe7626
Mahmudul-Hasan-dev <Contact.mahmudul.h@gmail.com>
2023-03-05 19:05:23 +0600

    isosilies

---
+++ isosilies.js
@@ -0,0 +1,10 @@
+var side1 = 10;
+var side2 = 12;
+var side3 = 10;
+
+if (side1 == side2 || side2 == side3 || side1 == side3) {
+    console.log('isosilies');
+}
+else {
+    console.log('negative');
+}
